a0285958771f0115f903a0e02d0d39c9c7c9b674
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-10-27 08:37:08 -0700

    Added accomadation for displaying change in values
    Uses TweenService for tweening and also nice accomodation js.

--- ClientEconomyHUD.lua
+++ ClientEconomyHUD.lua
@@ -5,18 +5,43 @@ local Bank = CurrencyFolder:WaitForChild("Bank")
 local CashDisplay = script.Parent:WaitForChild("CashDisp")
 local BankDisplay = script.Parent:WaitForChild("BankDisp")
 
+local BankChange = script.Parent:WaitForChild("BankChangeDisp")
+local CashChange = script.Parent:WaitForChild("CashChangeDisp")
+
 CashDisplay.Text = "$"..Cash.Value
 BankDisplay.Text = "$"..Bank.Value
 
-Cash.Changed:Connect(function()
+function TweenChanger(display, othervalue)
   
-  CashDisplay.Text = "$"..Cash.Value
+  display.Text = othervalue
+  local TS = game:GetService("TweenService")
+  
+  local tweeninfoish = TweenInfo.new(
+    
+    1,
+    Enum.EasingStyle.Cubic,
+    Enum.EasingDirection.In,
+    0,
+    false
+    
+  )
+  
+  local tween = TS:Create(display, tweeninfoish, {TextTransperency = 1})
+  tween:Play()
   
 end)
 
-Bank.Changed:Connect(function()
+Cash.Changed:Connect(function(change)
+  
+  CashDisplay.Text = "$"..Cash.Value
+  TweenChanger(CashChange, "+"..change)
+  
+end)
+
+Bank.Changed:Connect(function(change)
   
   BankDisplay.Text = "$"..Bank.Value
+  TweenChanger(BankChange, "-"..change)
   
 end)
 
