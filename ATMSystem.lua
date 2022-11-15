93a2de5ca6cb5070b5c26575d5adbc3e911782c5
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-15 10:22:00 -0800

    Update ATMSystem.lua

--- ATMSystem.lua
+++ ATMSystem.lua
@@ -1,8 +1,14 @@
 -- Server Script
 local Prompt = script.Parent
 
-local EquipRE = game:GetService("ReplicatedStorage"):WaitForChild("GameStuff"):WaitForChild("Remote"):WaitForChild("ATMSystem")
+local ShowATMRE = game:GetService("ReplicatedStorage"):WaitForChild("GameStuff"):WaitForChild("Remote"):WaitForChild("ATMSystem")
 
 Prompt.Triggered:Connect(function(plr)
     
+   ShowATMRE:FireClient(plr)
+   
+   local ForceFLD = Instance.new("ForceField")
+   ForceFLD.Parent = plr.Character
+   
+        
 end)
