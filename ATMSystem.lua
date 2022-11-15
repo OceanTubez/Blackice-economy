3bbb36a9b66d4420c5b8b0a03cab845c12305546
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-15 08:59:41 -0800

    Created base file

---
+++ ATMSystem.lua
@@ -0,0 +1,8 @@
+-- Server Script
+local Prompt = script.Parent
+
+local EquipRE = game:GetService("ReplicatedStorage"):WaitForChild("GameStuff"):WaitForChild("Remote"):WaitForChild("ATMSystem")
+
+Prompt.Triggered:Connect(function(plr)
+    
+end)
