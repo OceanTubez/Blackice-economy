18362078bddd8c8a8cd52c59dd93d3f897f3092b
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-16 11:12:30 -0800

    Completed script, adding changes later

--- Safe-System.lua
+++ Safe-System.lua
@@ -3,6 +3,7 @@ local HealthTopVal = Mainmodel:WaitForChild("MaxHealth")
 local Health = Mainmodel:WaitForChild("Health")
 local RespawnTime = Mainmodel:WaitForChild("RespawnDelay")
 local IsBroken = Mainmodel:WaitForChild("IsBroken")
+local FX = Mainmodel:WaitForChild("FX")
 
 local TweenService = game:GetService("TweenService")
 
@@ -25,20 +26,24 @@ IsBroken.Changed:Connect(function()
     if currentval == true then
             
         local NewTween = TweenService:Create(Door, Tweeninfobruv, {Orientation = blahblahblah})
-        
+        FX.Enabled = true
+            
         NewTween:Play()
         NewTween.Completed:Wait()
+        
+        
 
         while task.wait(RespawnTime.Value) then
 
             IsBroken.Value = false
             Health = HealthTopVal.Value
+            local newtween = TweenService:Create(Door, Tweeninfobruv, {Orientation = blahblahblah})
+            newtween:Play()
+            FX.Enabled = false
 
         end
 
     end
 
 end)
-
-
 
