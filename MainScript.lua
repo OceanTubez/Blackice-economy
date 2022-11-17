8004d7987c5945ff4412bb05911f6dc8fc274c81
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-17 09:37:56 -0800

    made core script

--- MainScript.lua
+++ MainScript.lua
@@ -4,3 +4,16 @@ local tool = script.Parent
 
 local extendedmap = tool:WaitForChild("ExtendedMap")
 local foldedMap = tool:WaitForChild("Handle")
+
+local equipanim = tool:WaitForChild("EquipAnimation")
+
+tool.Equipped:Connect(function()
+
+    local char = tool.Parent
+    local humanoid = char:WaitForChild("Humanoid")
+
+    local loadanim = humanoid:LoadAnimation(equipanim)
+    loadanim:Play()
+    
+    
+end)
