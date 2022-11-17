84b2d9dd3aa6045bb9662dcc5dc39e75ab2712ce
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-17 10:55:30 -0800

    Completed Lockpick script
    lullololeuleuelellelele

--- MainScript.lua
+++ MainScript.lua
@@ -2,22 +2,33 @@
 local tool = script.Parent
 
 local tries = tool:WaitForChild("Tries")
+local LockpickRE = tool:WaitForChild("LockpickRE")
+
+local highlights = {}
 
 tool.Equipped:Connect(function()
 
     local allsafes = game:GetService("Workspace"):WaitForChild("Locations"):WaitForChild("Safes")
 
-    local highlights = {}
-    
-    for i, safe in ipairs(allsafes) do
+    local char = tool.Parent
+    local HRP = char:WaitForChild("HumanoidRootPart")
 
-        local hitbox = safe:WaitForChild("HitBox")
-        local highlight = Instance.new("Highlight")
-        highlight.FillTransparency = 1
-        highlight.Adornee = hitbox
-        highlight.OutlineColor = Color3.fromRGB(0, 255, 0)
-        highlight.Enabled = true
-        highlights[i] = highlight
+    while task.Wait(0.5) do
+
+        for i, safe in ipairs(allsafes) do
+
+            if (HRP.Position - safe.Hitbox).Magnitude < 15 then
+
+                local high = Instance.new("Highlight")
+                high.FillColor = Color3.fromRGB(0, 218, 242)
+                high.OutlineColor = Color3.fromRGB(0, 255, 0)
+                high.Parent = safe
+                
+                highlights[i] = high
+
+            end
+
+        end
 
     end
 
@@ -28,6 +39,23 @@ tool.UnEquipped:Connect(function()
     for i, highlight in ipairs(highlights) do
 
         highlight:Destroy()
+
+    end
+
+end)
+
+LockpickRE.OnServerEvent:Connect(function(targetedpart)
+
+    local safes = game:GetService("Workspace"):WaitForChild("Locations"):WaitForChild("Safes"):GetChildren()
+
+    for i, safe in ipairs(safes) do
+
+        if targetedpart.Parent == safe then
+
+            tries -= 1
+            safe.Health -= 100
+
+        end
 
     end
 
