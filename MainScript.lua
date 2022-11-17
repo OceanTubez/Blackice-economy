27b520b32b163f772a0c838a1515ada6fcf7718e
Gurarman Dhanoa <84045281+OceanTubez@users.noreply.github.com>
2022-11-17 08:49:40 -0800

    CHANGED SCRIPT OOGA BOOGA

--- MainScript.lua
+++ MainScript.lua
@@ -1,34 +1,6 @@
--- should be in the lockpick
+-- map
+
 local tool = script.Parent
 
-local tries = tool:WaitForChild("Tries")
-
-tool.Equipped:Connect(function()
-
-    local allsafes = game:GetService("Workspace"):WaitForChild("Locations"):WaitForChild("Safes")
-
-    local highlights = {}
-    
-    for i, safe in ipairs(allsafes) do
-
-        local hitbox = safe:WaitForChild("HitBox")
-        local highlight = Instance.new("Highlight")
-        highlight.FillTransparency = 1
-        highlight.Adornee = hitbox
-        highlight.OutlineColor = Color3.fromRGB(0, 255, 0)
-        highlight.Enabled = true
-        highlights[i] = highlight
-
-    end
-
-end)
-
-tool.UnEquipped:Connect(function()
-
-    for i, highlight in ipairs(highlights) do
-
-        highlight:Destroy()
-
-    end
-
-end)
+local extendedmap = tool:WaitForChild("ExtendedMap")
+local foldedMap = tool:WaitForChild("Handle")
